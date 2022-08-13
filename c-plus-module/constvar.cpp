53b4b2b49586e1794cb9b3a3fde0ac5579df0010
yangxin <1393003255@qq.com>
2022-08-13 07:55:33 -0400

    feat: C++常量

---
+++ c-plus-module/constvar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
+
+int main()
+{
+    int area;
+
+    area = LENGTH * WIDTH;
+    cout << area << endl;
+    cout << NEWLINE << endl;
+
+    return 0;
+}
